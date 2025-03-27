b2e81934fbbd57b39e06217d597b152f3ae716e8
James Biggs <james.biggs@nationalarchives.gov.uk>
2025-03-27 16:27:46 +0000

    Add RedisCache to CACHES in base.py

--- wagtailshop/settings/base.py
+++ wagtailshop/settings/base.py
@@ -92,6 +92,16 @@ DATABASES = {
     }
 }
 
+# Cache
+# https://docs.djangoproject.com/en/5.1/topics/cache/
+
+if redis_url := os.getenv("REDIS_URL"):
+    CACHES = {
+        "default": {
+            "BACKEND": "django.core.cache.backends.redis.RedisCache",
+            "LOCATION": redis_url,
+        }
+    }
 
 # Password validation
 # https://docs.djangoproject.com/en/5.1/ref/settings/#auth-password-validators
